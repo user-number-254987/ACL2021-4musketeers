5a8309d921119c0b2426b91e4834afc3cddaa071
alaadine123 <mohamedalaaddine4@gmail.com>
2022-01-10 01:18:41 +0100

    Delete module info-java

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ensem.gameACL2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires javafx.graphics;
-
-    opens ensem.gameACL2 to javafx.fxml;
-    exports ensem.gameACL2;
-}
